e4e526b1c517cfb14b071eff6479043ea7f8094a
democvidev <cnvvc_vlad@mail.ru>
2021-06-21 11:46:38 +0200

    home page update

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,12 +9,27 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="home_index")
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig', []);
     }
+
+    /**
+     * @Route("/agenda", name="home_agenda")
+     */
+    public function agenda(): Response
+    {
+        return $this->render('home/agenda.html.twig', []);
+    }
+
+    /**
+     * @Route("/billeterie", name="home_billeterie")
+     */
+    public function billeterie(): Response
+    {
+        return $this->render('home/billeterie.html.twig', []);
+    }
+
 }
