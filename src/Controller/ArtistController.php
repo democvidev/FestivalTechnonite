9aa3db80a555c7f1dd8965740fd7753981a629b7
democvidev <cnvvc_vlad@mail.ru>
2021-06-21 22:19:29 +0200

    artist controller

---
+++ src/Controller/ArtistController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArtistController extends AbstractController
+{
+    /**
+     * @Route("/artist", name="artist_home")
+     */
+    public function home(): Response
+    {
+        return $this->render('artist/home.html.twig', []);
+    }
+
+    /**
+     * @Route("/artist/{id}", name="artist_view", methods={"GET"}, requirements={"id"="\d+"})
+     */
+    public function artist(): Response
+    {
+        return $this->render('artist/artist.html.twig', []);
+    }
+}
