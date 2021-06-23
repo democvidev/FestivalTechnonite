c9eebfcfe1a332000ebc2716c16a0bce9e5fc209
cvidev <cnvvc_vlad@mail.ru>
2021-06-23 14:35:34 +0200

    updated navigation

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -30,6 +30,7 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, UserAuthenticator $authenticator): Response
     {
+        
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
