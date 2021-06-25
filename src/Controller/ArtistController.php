ec973f3142e2c63058cb2af2f1eb8172d6567418
democvidev <cnvvc_vlad@mail.ru>
2021-06-25 20:46:56 +0200

    knp paginator bundle

--- src/Controller/ArtistController.php
+++ src/Controller/ArtistController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Artist;
 use App\Repository\ArtistRepository;
 use App\Repository\CategoryRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,11 +16,16 @@ class ArtistController extends AbstractController
     /**
      * @Route("/artist", name="artist_home")
      */
-    public function home(CategoryRepository $categoryRepository, ArtistRepository $artistRepository): Response
+    public function home(CategoryRepository $categoryRepository, ArtistRepository $artistRepository, Request $request, PaginatorInterface $paginator): Response
     {
         // recupère le tableau remplie des objets par injection de dépendances
-        $artists = $artistRepository->findAll();
+        $data = $artistRepository->findAll();
         $categories = $categoryRepository->findAll(); 
+        $artists = $paginator->paginate(
+            $data,
+            $request->query->get('page', 1), // nr de la page en cours, par défaut la page 1
+            9 // artists par page
+        );
 
         // dd($artists);
 
@@ -43,10 +50,15 @@ class ArtistController extends AbstractController
     /**
      * @Route("/artist/category/{id}", name="artist_view_by_category", methods={"GET"}, requirements={"id"="\d+"})
      */
-    public function viewByCategory($id, ArtistRepository $artistRepository, CategoryRepository $categoryRepository): Response
+    public function viewByCategory($id, ArtistRepository $artistRepository, CategoryRepository $categoryRepository, Request $request, PaginatorInterface $paginator): Response
     {
-        $artists = $artistRepository->findByCategory($id);
+        $data = $artistRepository->findByCategory($id);
         $categories = $categoryRepository->findAll();
+        $artists = $paginator->paginate(
+            $data,
+            $request->query->get('page', 1), // nr de la page en cours, par défaut la page 1
+            9 // artists par page
+        );
         // dd($artists);
         return $this->render('artist/home.html.twig', [
             'artists' => $artists,
