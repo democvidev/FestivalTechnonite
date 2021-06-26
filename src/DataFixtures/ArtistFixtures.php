5fb46660681c941b1bdfa6d4f062d80fe7255e86
democvidev <cnvvc_vlad@mail.ru>
2021-06-27 01:01:57 +0200

    updated fixtures && add return button

--- src/DataFixtures/ArtistFixtures.php
+++ src/DataFixtures/ArtistFixtures.php
@@ -15,22 +15,24 @@ class ArtistFixtures extends Fixture implements DependentFixtureInterface
     {
         $faker = Factory::create('fr_FR');
 
-
-        for($nbArtists = 1; $nbArtists <= 100; $nbArtists++ ) {
-            $category = $this->getReference('category_' . $faker->numberBetween(1, 5));
-            $artist = new Artist;
+        for ($nbArtists = 1; $nbArtists <= 15; $nbArtists++) {
+            $category = $this->getReference(
+                'category_' . $faker->numberBetween(1, 5)
+            );
+            $artist = new Artist();
             // 2 fois sur 10 il y aura des aristes sans categorie
-            if(rand(1, 100) >= 20){
+            if (rand(1, 100) >= 20) {
                 $artist->setCategory($category);
             } else {
-                $artist->setCategory(NULL);
+                $artist->setCategory(null);
             }
-            
-            // 1 fois sur 10 il y aura des aristes sans concert
-            if(rand(1, 100) >= 10){
-                $artist->setConcert($faker->numberBetween(1, 9));
+
+            // 1 fois sur 10 il y aura des aristes sans concert programé
+            // if(rand(1, 100) >= 10){
+            if ($nbArtists <= 9) {
+                $artist->setConcert($nbArtists);
             } else {
-                $artist->setConcert(NULL);
+                $artist->setConcert(null);
             }
             $artist->setName($faker->lastName);
             $artist->setDescription($faker->realText(5000));
@@ -42,8 +44,6 @@ class ArtistFixtures extends Fixture implements DependentFixtureInterface
     public function getDependencies()
     {
         // retourne la liste des dépendances du notre objet, qui doivent s'exécuter avant
-        return [
-            CategoryFixtures::class            
-        ];
+        return [CategoryFixtures::class];
     }
 }
