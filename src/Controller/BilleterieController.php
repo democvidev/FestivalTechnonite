f225e86aed3ec61a1212279e415b5263c6a1f2c6
democvidev <cnvvc_vlad@mail.ru>
2021-06-26 11:30:05 +0200

    swift mailer manage form reservation

--- src/Controller/BilleterieController.php
+++ src/Controller/BilleterieController.php
@@ -6,6 +6,8 @@ use App\Form\BilleterieFormType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class BilleterieController extends AbstractController
 {
@@ -14,32 +16,58 @@ class BilleterieController extends AbstractController
      */
     public function agenda(): Response
     {
-
-        $agendaList =[];
-        for($i = 0; $i < 9; $i++) {
+        $agendaList = [];
+        for ($i = 0; $i < 9; $i++) {
             $row = [
                 'Date' => '20/08/2021',
                 'Time' => '16h-18h',
                 'Artist' => 'artist n°' . $i,
-                'Reservation' => 'Reserver une place'
+                'Reservation' => 'Reserver une place',
             ];
             $agendaList[] = $row;
         }
         return $this->render('billeterie/agenda.html.twig', [
-            'agendaList' => $agendaList
+            'agendaList' => $agendaList,
         ]);
-    }    
+    }
 
-     /**
+    /**
      * @Route("/billeterie", name="billeterie_form")
      */
-    public function form(): Response
-    {
+    public function form(
+        Request $request,
+        \Swift_Mailer $mailer,
+        UserInterface $user
+    ): Response {
         $form = $this->createForm(BilleterieFormType::class);
-       
-        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            //fabriquation du mail
+            $message = (new \Swift_Message('Nouvelle Réservation'))
+                //hydratation d'email
+                ->setFrom($user->getEmail())
+                ->setTo('admin@admin.com')
+                ->setBody(
+                    $this->renderView('billeterie/email.html.twig', [
+                        'data' => $data,
+                    ]),
+                    'text/html'
+                );
+            //envoie du message
+            $mailer->send($message);
+
+            $this->addFlash(
+                'success',
+                'Votre réservation a bien été envoyée !'
+            );
+            return $this->redirectToRoute('home_index');
+        }
+
         return $this->render('billeterie/form.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
         ]);
-    }    
+    }
 }
